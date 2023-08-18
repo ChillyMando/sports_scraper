47f9e666e745706dec6536dc7a5d1fec39a65f63
Colby Falk <colbyfalk95@gmail.com>
2023-08-17 23:37:26 -0500

    Added parsing to divide up odds into 2d lists

--- main.py
+++ main.py
@@ -1,11 +1,125 @@
 import os
 import time
-# import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
+
+# TO-DO Simplify parsing functions
+
+
+def moneyline_parse():
+    for item_index, item in enumerate(parse_list):
+        if item == 'Money Line':
+            if parse_list[item_index + 3] != 'N/A':
+                odds = []
+                odds.append(parse_list[item_index + 3])
+                odds.append(float(parse_list[item_index + 4]))
+                odds.append(parse_list[item_index + 5])
+                odds.append(float(parse_list[item_index + 6]))
+                odds.append(1/odds[1] + 1/odds[3] - 1)
+                if parse_list[item_index + 7] != 'N/A' and parse_list[item_index + 13] != 'N/A':
+                    try:
+                        odds.append(parse_list[item_index + 19])
+                    except IndexError:
+                        pass
+                moneyline.append(odds)
+
+
+def spread_parse():
+    for item_index, item in enumerate(parse_list):
+        if item == 'Spread':
+            if parse_list[item_index + 2] == 'N/A':
+                if parse_list[item_index + 4] != 'N/A':
+                    odds = []
+                    odds.append(parse_list[item_index + 4])
+                    odds.append(float(parse_list[item_index + 5]))
+                    odds.append(float(parse_list[item_index + 6]))
+                    odds.append(parse_list[item_index + 7])
+                    odds.append(float(parse_list[item_index + 8]))
+                    odds.append(float(parse_list[item_index + 9]))
+                    odds.append(1/odds[2] + 1/odds[5] - 1)
+                    if parse_list[item_index + 10] != 'N/A':
+                        try:
+                            odds.append(parse_list[item_index + 16])
+                        except IndexError:
+                            pass
+                    else:
+                        try:
+                            odds.append(parse_list[item_index + 13])
+                        except IndexError:
+                            pass
+                    spread.append(odds)
+            elif parse_list[item_index + 6] != 'N/A':
+                odds = []
+                odds.append(parse_list[item_index + 6])
+                odds.append(float(parse_list[item_index + 7]))
+                odds.append(float(parse_list[item_index + 8]))
+                odds.append(parse_list[item_index + 9])
+                odds.append(float(parse_list[item_index + 10]))
+                odds.append(float(parse_list[item_index + 11]))
+                odds.append(1/odds[2] + 1/odds[5] - 1)
+                if parse_list[item_index + 12] != 'N/A':
+                    try:
+                        odds.append(parse_list[item_index + 18])
+                    except IndexError:
+                        pass
+                else:
+                    try:
+                        odds.append(parse_list[item_index + 14])
+                    except IndexError:
+                        pass
+                spread.append(odds)
+
+
+def total_parse():
+    for item_index, item in enumerate(parse_list):
+        if item == 'Total':
+            if parse_list[item_index + 1] == 'N/A':
+                if parse_list[item_index + 3] == 'N/A':
+                    if parse_list[item_index + 5] != 'N/A':
+                        odds = []
+                        odds.append(parse_list[item_index - 4])
+                        odds.append(parse_list[item_index - 3])
+                        odds.append(float(parse_list[item_index + 6]))
+                        odds.append(float(parse_list[item_index + 7]))
+                        odds.append(float(parse_list[item_index + 10]))
+                        odds.append(1/odds[3] + 1/odds[4] - 1)
+                        odds.append(parse_list[item_index + 11])
+                        total.append(odds)
+                elif parse_list[item_index + 9] != 'N/A':
+                    odds = []
+                    odds.append(parse_list[item_index - 4])
+                    odds.append(parse_list[item_index - 3])
+                    odds.append(float(parse_list[item_index + 10]))
+                    odds.append(float(parse_list[item_index + 11]))
+                    odds.append(float(parse_list[item_index + 14]))
+                    odds.append(1/odds[3] + 1/odds[4] - 1)
+                    odds.append(parse_list[item_index + 15])
+                    total.append(odds)
+            elif parse_list[item_index + 5] == 'N/A':
+                if parse_list[item_index + 7] != 'N/A':
+                    odds = []
+                    odds.append(parse_list[item_index - 4])
+                    odds.append(parse_list[item_index - 3])
+                    odds.append(float(parse_list[item_index + 8]))
+                    odds.append(float(parse_list[item_index + 9]))
+                    odds.append(float(parse_list[item_index + 12]))
+                    odds.append(1/odds[3] + 1/odds[4] - 1)
+                    odds.append(parse_list[item_index + 13])
+                    total.append(odds)
+            elif parse_list[item_index + 11] != 'N/A':
+                odds = []
+                odds.append(parse_list[item_index - 4])
+                odds.append(parse_list[item_index - 3])
+                odds.append(float(parse_list[item_index + 12]))
+                odds.append(float(parse_list[item_index + 13]))
+                odds.append(float(parse_list[item_index + 16]))
+                odds.append(1/odds[3] + 1/odds[4] - 1)
+                odds.append(parse_list[item_index + 17])
+                total.append(odds)
+
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -24,5 +138,25 @@ show_more = driver.find_elements(
 for element in show_more:
     element.send_keys(Keys.RETURN)
 
-text = body.text
-print(text)
+text = body.text.split('\n')
+parse_list = []
+for line in text:
+    parse_list.append(line)
+
+# driver.quit()
+
+del (parse_list[0:(parse_list.index('Money Line') - 3)])
+del (parse_list[parse_list.index('Outrights and Futures')-1:])
+
+moneyline = [['away', 'odds', 'home', 'odds', 'vig', 'date']]
+moneyline_parse()
+
+spread = [['away', 'spread', 'odds', 'home', 'spread', 'odds', 'date']]
+spread_parse()
+
+total = [['away', 'home', 'score' 'over_odds', 'under_odds', 'vig', 'date']]
+total_parse()
+
+print('Money Line: \n', moneyline, '\n')
+print('Spread: \n', spread, '\n')
+print('Totals: \n', total, '\n')
